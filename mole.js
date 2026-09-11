3103f864711ca0cfcce8f4953091c3d04c6a86a0
Cody M <flameking374@gmail.com>
2026-09-11 09:00:36 -0400

    Tutorial project complete

---
+++ mole.js
@@ -0,0 +1,80 @@
+let currMoleTile;
+let currPlantTile;
+let score = 0;
+let gameOver = false;
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    //Set up the grid for the game board in html
+    for (let i = 0; i < 9; i++) {
+        //<div id="0-8"></div>
+        let tile = document.createElement("div");
+        tile.id = i.toString();
+        tile.addEventListener("click", selectTile)
+        document.getElementById("board").appendChild(tile);
+    }
+
+    setInterval(setMole, 1000); //1,000 milliseconds or 1 seconds
+    setInterval(setPlant, 2000); //2,000 milliseconds or 2 seconds
+}
+
+/* Picks a random tile 0-8 */
+function getRandomTile() {
+    let num = Math.floor(Math.random() * 9)
+    return num.toString();
+}
+
+/* Sets the mole to appear at a random tile */
+function setMole() {
+    if (gameOver) {
+        return;
+    }
+
+    if (currMoleTile) {
+        currMoleTile.innerHTML = "";
+    }
+
+    let mole = document.createElement("img");
+    mole.src = "./media/monty-mole.png"
+
+    let num = getRandomTile();
+    if (currPlantTile && currPlantTile.id == num) {
+        return;
+    }
+    currMoleTile = document.getElementById(num);
+    currMoleTile.appendChild(mole);
+}
+
+function setPlant() {
+    if (gameOver) {
+        return;
+    }
+    
+    if (currPlantTile) {
+        currPlantTile.innerHTML = "";
+    }
+
+    let plant = document.createElement("img");
+    plant.src = "./media/piranha-plant.png"
+
+    let num = getRandomTile();
+    if (currMoleTile && currMoleTile.id == num) {
+        return;
+    }
+    currPlantTile = document.getElementById(num);
+    currPlantTile.appendChild(plant);
+}
+
+function selectTile() {
+    if (this == currMoleTile) {
+        score += 10;
+        document.getElementById("score").innerText = score.toString();
+    }
+    else if (this == currPlantTile) {
+        document.getElementById("score").innerText = "GAME OVER: " + score.toString();
+        gameOver = true;
+    }
+}
